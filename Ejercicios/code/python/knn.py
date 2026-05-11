e207e61c1defb72acab51bceecc478beb2bb1517
Oliver Calderon <L22170590@culiacan.tecnm.mx>
2026-05-11 01:10:01 -0600

    Subiendo Ejercicios

---
+++ Ejercicios/code/python/knn.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
